389ebf17b4321e457243e7ef61727b95430e274d
Misigno <efournier92@gmail.com>
2017-01-09 18:53:19 -0500

    fix folder_string bug

--- audiomator.rb
+++ audiomator.rb
@@ -5,16 +5,17 @@ def directory_exists?(directory)
 end
 
 if directory_exists?(Dir['/Volumes/RECORD/RECORD/VOICE/*'])
-  folder_string = '/Volumes/RECORD/RECORD/VOICE/'
+  folder_string = '/Volumes/RECORD/RECORD/VOICE/*'
   stop_char     = 29
 elsif directory_exists?(Dir['/Volumes/RECORD/RECORD/*'])
-  folder_string = '/Volumes/RECORD/RECORD/'
+  folder_string = '/Volumes/RECORD/RECORD/*'
   stop_char     = 23
 else
   abort('No files found...')
 end
 
 audio_files = Dir[folder_string] 
+  binding.pry
 filtered = audio_files.select do |file|
   file !~ /_/
 end
@@ -31,10 +32,10 @@ end
 dates.each do |date|
   i = 2
   out_file = File.new("/Volumes/M_EXTENDED/REC/ls/#{date}.txt", "w")
-  out_file.puts("file #{file_string}#{date}.WAV'")
+  out_file.puts("file #{folder_string}#{date}.WAV'")
 
   while i <= 9 do
-    out_file.puts("file #{file_string}#{date}_0#{i}.WAV'")
+    out_file.puts("file #{folder_string}#{date}_0#{i}.WAV'")
     i+=2
   end
   out_file.close
